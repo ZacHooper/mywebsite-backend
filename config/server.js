2ee50743f6740e65222c1e9a9e6889f26aac09ef
ZacHooper <zac.g.hooper@gmail.com>
2021-09-18 15:24:26 +1000

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '22498258d5e964ef7b9c06982c039b07'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1008ffba-a3bf-4979-a9b2-e31c4cd2644a'
+};
